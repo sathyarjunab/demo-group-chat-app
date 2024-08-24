9a5415c114b483625e5f2bc1db976659d1a2d2ab
sathyarjunab <sathyarjun007@gmail.com>
2024-08-24 12:24:25 +0530

    login front end

--- login.js
+++ login.js
@@ -1,44 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
   document
-    .getElementById("signupForm")
+    .getElementById("loginForm")
     .addEventListener("submit", async function (event) {
       event.preventDefault();
 
-      const name = document.getElementById("name").value;
       const email = document.getElementById("email").value;
-      const phone = document.getElementById("phone").value;
       const password = document.getElementById("password").value;
 
       const data = {
-        name: name,
         email: email,
-        phone: phone,
         password: password,
       };
 
       try {
-        const response = await fetch("http://localhost:3000/signup", {
+        const response = await fetch("http://localhost:3000/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
         });
-        const resdata = await response.json();
+
         if (response.ok) {
-          alert("Sign up successful!");
-          window.location.href = "/login.html";
-          document.getElementById("message").style.color = "green";
-          document.getElementById("signupForm").reset();
+          document.getElementById("loginForm").reset();
+          alert("login successful!");
         } else {
-          console.log(resdata);
-          document.getElementById("message").textContent = resdata.message;
-          alert(resdata.message);
+          document.getElementById("message").textContent = response.message;
+          document.getElementById("message").style.color = "red";
         }
       } catch (error) {
-        console.log(error);
         document.getElementById("message").textContent =
           "An error occurred. Please try again.";
+        document.getElementById("message").style.color = "red";
       }
+    });
+  document
+    .getElementsByClassName("forgotpassbtn")
+    .addEventListener("click", async () => {
+      const response = await fetch("http://localhost:3000/forgotPassword");
     });
 });
